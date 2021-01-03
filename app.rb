8c4eb636717f967ca93f0cda44d082d08f5a23e1
Hunter <kolasa.hunter@aol.com>
2021-01-02 23:51:18 -0500

    commit tree cb75e4bc30d0cc85b6b827e9836ceeb54e06574b

--- app.rb
+++ app.rb
@@ -1,6 +1,58 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
-  # Write your code here!
+  get '/reversename/:name' do
+    @name = params[:name].reverse
+    "#{@name}"
+  end
+
+  get '/square/:number' do
+    @square = params[:number].to_i ** 2
+    "#{@square.to_s}"
+  end
+
+  get '/say/:number/:phrase' do
+    number = params[:number].to_i
+    phrase = params[:phrase]
+    repeated_phrase = ''
+    
+    number.times do 
+      repeated_phrase += phrase
+    end
+    repeated_phrase
+  end
+
+  get '/say/:word1/:word2/:word3/:word4/:word5' do
+    counter = 0
+    @phrase = ''
+    4.times do
+      counter += 1
+      @word = "word" + counter.to_s
+      @phrase += params[@word] + " "
+    end
+    @phrase += params[:word5] + "."
+    @phrase
+  end
+
+  get '/:operation/:number1/:number2' do
+    @operation = params[:operation]
+    @num1 = params[:number1].to_i
+    @num2 = params[:number2].to_i
+
+    case @operation
+    when "add"
+      @answer = @num1 + @num2
+    when "subtract"
+      @answer = @num1 - @num2
+    when "multiply"
+      @answer = @num1 * @num2
+    when "divide"
+      @answer = @num1 / @num2
+    end
+
+    @answer.to_s
+  end
+
+
 
 end
